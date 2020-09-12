9f565c15696494cca48cb23dd827be4dd8b9514d
Caroline Santos <caroline.santos1172@gmail.com.br>
2020-09-12 14:42:13 -0300

    insercao dos metodos gets

---
+++ usuarios/get-usuarios.spec.js
@@ -0,0 +1,18 @@
+const testServer = require('../utils/testServer')
+
+const rotaUsuarios = '/usuarios'
+
+describe('Listar usuário através da rota GET', () => {
+    it.only('Listar usuários', async () => {    
+        const response = await testServer.get(rotaUsuarios)
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('quantidade');
+        expect(response.body).toHaveProperty('usuarios');
+        expect(response.body.usuarios[0]).toHaveProperty('nome');
+        expect(response.body.usuarios[0]).toHaveProperty('email');
+        expect(response.body.usuarios[0]).toHaveProperty('password');
+        expect(response.body.usuarios[0]).toHaveProperty('administrador');
+        expect(response.body.usuarios[0]).toHaveProperty('_id');
+      })
+
+})
